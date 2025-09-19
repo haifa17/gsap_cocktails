bd7587496459b7dc7cc2daeac71e30de8402c47e
haifa khiari <haifa.khiari.visioad@gmail.com>
2025-09-19 12:04:27 +0100

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+export default function Home() {
+  return <div className="flex-center h-[100vh]">hiiiiiiiii</div>;
+}
