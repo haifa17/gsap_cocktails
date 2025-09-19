726b93303a48e61d0fa3392b70a0dc329d61204c
haifa khiari <haifa.khiari.visioad@gmail.com>
2025-09-19 17:07:27 +0100

    implement menu section

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 import About from "@/components/About";
 import Art from "@/components/Art";
 import Cocktails from "@/components/Cocktails";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
+import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
@@ -18,6 +19,7 @@ export default function Home() {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
       <Contact />
     </div>
   );
